e2223d8aa7210c10fe611cfc4d35a5aace59ca3b
MexZin <mik130598@gmail.com>
2021-03-09 23:05:35 +0400

    N

--- Week 1/Lesson1/src/com/company/Main.java
+++ Week 1/Lesson1/src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 //        task3 task3 = new task3();
 //        Task6 task6 = new Task6();
-        task12 task12 = new task12();
+//        task12 task12 = new task12();
+            task1 task = new task1();
     }
 }
---
+++ Week 1/Lesson1/src/com/company/task1.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class task1 {
+    public task1(){
+        System.out.println("C++");
+        System.out.println("C#");
+        System.out.println("Java");
+        System.out.println("JavaScript");
+    }
+}
